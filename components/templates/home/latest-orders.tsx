dc4d0074e81b4c0dfa9a716e9d386eed2846ace8
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-21 19:23:26 +0330

    Added Latest Orders Component

---
+++ components/templates/home/latest-orders.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LatestOrders = () => {
+  return (
+    <div className='p-4 border rounded-2xl border-zinc-200'>
+        <div className="flex items-center justify-between">
+            <h4 className='text-lg font-bold'>
+                آخرین سفارشات
+            </h4>
+        </div>
+    </div>
+  )
+}
+
+export default LatestOrders
